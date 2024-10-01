d18bba8e3d3c371f17919bb79130cd3e503d6fbd
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-09-30 20:51:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mi_paquete',
+    name='paquete',
     version='0.1.0',
     packages=find_packages(),
     description='Descripción breve del paquete',
-    author='G',
-    author_email='tu_email@example.com',
-    url='URL del repositorio de GitHub',
+    author='JR',
+    author_email='jrha@ier.unam.mx',
+    url='https://github.com/JRamonHA/paquete',
     install_requires=[
         # Lista de dependencias necesarias
     ],
